665c3bd10d044f297233a6adead4e0243e99476c
yaswanth <119861381+aH0-STS@users.noreply.github.com>
2025-06-02 16:00:06 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,8 +1,8 @@
 def call(String imageName, String credId) {
     withCredentials([usernamePassword(credentialsId: credId, passwordVariable: 'Password', usernameVariable: 'Docker')]) {
-        sh '''
-        echo "$Password" | docker login -u "$Docker" --password-stdin
+        sh """
+        echo "\$Password" | docker login -u "\$Docker" --password-stdin
         docker push ${imageName}:latest
-        '''
+        """
     }
 }
